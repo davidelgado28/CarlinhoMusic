5cc49491c52c2bea1a4445fb88b29a7ef94f52ba
David Carlos <davidcarlosdelgado28@gmail.com>
2026-09-06 20:05:10 -0300

    Update Track.php

--- app/Models/Track.php
+++ app/Models/Track.php
@@ -1,1 +1,42 @@
+<?php
 
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class User extends Authenticatable
+{
+    use HasApiTokens, HasFactory, Notifiable;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ];
+
+    public function playlists(): HasMany
+    {
+        return $this->hasMany(Playlist::class);
+    }
+
+    public function listeningHistories(): HasMany
+    {
+        return $this->hasMany(ListeningHistory::class);
+    }
+}
