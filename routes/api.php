987a3e15e2432590fb7e06b6b1c87b1b80f336bc
David Carlos <davidcarlosdelgado28@gmail.com>
2026-09-06 19:56:11 -0300

    Add API routes for track streaming and progress reporting

--- routes/api.php
+++ routes/api.php
@@ -1,1 +1,9 @@
+<?php
 
+use App\Http\Controllers\Api\TrackController;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware('auth:sanctum')->prefix('v1')->group(function () {
+    Route::get('/tracks/stream/{externalId}', [TrackController::class, 'stream']);
+    Route::post('/tracks/{trackId}/progress', [TrackController::class, 'reportProgress']);
+});
